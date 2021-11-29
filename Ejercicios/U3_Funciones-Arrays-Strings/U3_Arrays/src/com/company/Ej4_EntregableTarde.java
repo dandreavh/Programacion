3bae0687e74de25e02ac3cc756b9c62124763389
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2021-11-29 10:04:48 +0100

    Buscaminas

--- Ejercicios/U3_Funciones-Arrays-Strings/U3_Arrays/src/com/company/Ej4_EntregableTarde.java
+++ Ejercicios/U3_Funciones-Arrays-Strings/U3_Arrays/src/com/company/Ej4_EntregableTarde.java
@@ -12,80 +12,62 @@ public class Ej4_EntregableTarde {
     //Para comprobar que el juego está correcto se mostrar el tablero del juego cada vez (hasta que el usuario encuentre una bomba).
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[][] tablero;
         int n;
-        int tipo;
+        char[][] tablero;
         String posicion;
         char fila;
         char columna;
-        int cantBombas = 0;
 
         System.out.println("Introduzca el tamaño del tablero");
         n = sc.nextInt();
         sc.nextLine();
+        tablero = new char[n][n];
+        
+        System.out.println("Indique una posición. Ejemplo: 1,0");
+        posicion = sc.nextLine();
+        fila = (posicion.charAt(0));
+        columna = posicion.charAt(2);
 
-        tablero = new String[n][n];
+        for (int i=0;i<tablero.length;i++) {
+            for (int j=0;j<tablero[i].length;j++) {
+                int numero = (int) (Math.random()*100+1);
 
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                tipo = (int) (Math.random()*(2-0)+0);
-                if(tipo==0){
-                    System.out.print(" - ");
-                } else{
-                    System.out.print(" X ");
+                if (numero>=50) {
+                    tablero[i][j] = 'X';
+                } else {
+                    tablero[i][j] = ' ';
                 }
+            }
+        }
+
+        for (int i=0;i<tablero.length;i++) {
+            for (int j=0;j<tablero[i].length;j++) {
+                System.out.print(tablero[i][j]);
             }
             System.out.println();
         }
 
-        boolean bomba = false;
-        do{
-            System.out.println("Indique una posición. Ejemplo: 1,0");
-            posicion = sc.nextLine();
-            fila = (posicion.charAt(0));
-            columna = posicion.charAt(2);
+        System.out.println("La posición "+(posicion)+" tiene " +buscaminas(fila,columna,tablero)+ " bombas, pruebe otra posición");
+    }
 
-            for (int i = 0; i < tablero.length; i++) {
-                for (int j = 0; j < tablero[i].length; j++) {
-                    fila -=1;
-                    columna-=1;
-                    if(estaDentro(fila, columna, n)){
-                        if(tablero[fila][columna]==" X "){
-                            cantBombas++;
+    public static int buscaminas (int fila, int col, char[][] tablero)  {
+        int minas = 0;
+
+        if (tablero[fila][col] =='X') {
+            return -1;
+        } else {
+            for (int i = fila-1 ; i<=fila+1 ;i++) {
+                for (int j = col-1; j <=col+1 ; j++) {
+                    if (i>=0 && j>=0 && i<tablero.length && j<tablero[i].length) {
+                        if (tablero[i][j] == 'X') {
+                            minas++;
                         }
                     }
-//                    if(tablero[fila-1][columna-1]==" X " || tablero[fila][columna]==" X " || tablero[fila+1][columna-1]==" X "
-//                        || tablero[fila+1][columna]==" X " || tablero[fila+1][columna+1]==" X " || tablero[fila-1][columna+1]==" X "
-//                            || tablero[fila][columna+1]==" X " || tablero[fila-1][columna]==" X "){
-//                        cantBombas++;
-//                    }
+
                 }
-            }
-            System.out.println("La posición "+(posicion)+" tiene " +cantBombas+ " bombas, pruebe otra posición");
-
-        } while (!hayBomba(bomba, tablero, fila, columna));
-
-        System.out.println("La posición "+(posicion)+" tiene una bomba, está muerto");
-
-    }
-
-    public static boolean hayBomba(boolean bomba, String[][] tablero, char fila, char columna) {
-        boolean hay = false;
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                if((i==fila && j==columna) && tablero[i][j].equals(" X ")){
-                    hay = true;
-                    break;
-                }
-
             }
         }
-        return hay;
+
+        return minas;
     }
-
-    public static boolean estaDentro(int dimension, int i, int j) {
-        return (i>=0 && i<=dimension && j>=0 && j<=dimension);
-    }
-
-
 }
