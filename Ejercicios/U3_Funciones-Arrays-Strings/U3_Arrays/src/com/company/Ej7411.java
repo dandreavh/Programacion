b60b94f49732426b45d670182b99a132992d4255
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2021-12-05 16:27:49 +0100

    Diagonal izqSup a drchaInf con X

--- Ejercicios/U3_Funciones-Arrays-Strings/U3_Arrays/src/com/company/Ej7411.java
+++ Ejercicios/U3_Funciones-Arrays-Strings/U3_Arrays/src/com/company/Ej7411.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Ej7411 {
     // Crea un programa que cree un array de 5x5 caracteres, lo rellene con puntos en todas las posiciones excepto
-    // aquellas en las que el número de fila es igual al número de columna, en las que pondrá un X.
+    // aquellas en las que el número de fila es igual al número de columna, en las que pondrá un X. (Diagonal)
     public static void main(String[] args) {
         String[][] cadena = new String[5][5];
 
